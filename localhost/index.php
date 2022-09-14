267790eda0756affc0f9f2963d62330d0b1f67ba
Andersone331091 <johndoe@example.com>
2022-09-14 09:26:06 +0300

    Урок 2. Конец строки.

--- localhost/index.php
+++ localhost/index.php
@@ -4,9 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?= "Новая страница"; ?></title>
 </head>
 <body>
-    <?php echo "lox"; ?>
+    <?php
+     echo date(DATE_RSS); 
+     ?>
+     <br>
+     <?php
+    echo 9
+    -
+    2
+    +
+    6;
+     ?>
+     <br>
+    <?php
+    if(mt_rand(0,1)){
+        ?>
+        <div style="color: blue">Синий</div>
+        <?php
+    } else {
+        ?>
+        <div style="color: red">Красный</div>
+<?php    
+}
+    ?>
 </body>
 </html>
